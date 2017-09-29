2e808a6caf70e0cdd4f2d03fadcfc57b7588d4f2
Adrien <adrien@arccoza.com>
2017-09-29 21:16:58 +0900

    Add basic knockout ViewModel with list search/filter

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,31 @@
-import knockout from 'knockout'
+import ko from 'knockout'
+var print = console.log.bind(console)
+print(ko.applyBindings)
 
 
+var places = [
+  'Durban',
+  'Tokyo',
+  'Bangkok',
+]
 
+function AppVM() {
+  this.search = ko.observable()
+  this.places = ko.observableArray(places)
+
+  this.placesSearch = ko.computed(() => {
+    var s = this.search()
+
+    if (s) {
+      return ko.utils.arrayFilter(this.places(), p => p.toUpperCase().indexOf(s.toUpperCase()) != -1)
+    }
+
+    return this.places()
+  })
+}
+
+window.addEventListener('load', function(ev) {
+
+  ko.applyBindings(new AppVM())
+
+})
