a6500359ca06a7f9f7ae8298acaf245795a6cacc
Adrien <adrien@arccoza.com>
2017-09-30 23:54:44 +0900

    Add utils module and `arrayDiff` fn

---
+++ src/js/utils.js
@@ -0,0 +1,15 @@
+export function arrayDiff(orig, mod, key) {
+  mod = new Map(mod.map(b => [key(b), [b, true]]))
+
+  var add = []
+  var rem = orig.filter(a => {
+    var v = mod.get(key(a))
+    if (v !== undefined)
+      return v[1] = false  // That's not a mistake, both set and return false.
+    else
+      return true
+  })
+  mod.forEach((v, k) => v[1] ? add.push(v[0]) : null)
+
+  return [rem, add]
+}
