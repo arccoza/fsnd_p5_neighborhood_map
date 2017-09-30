6cf3066494e8442486d4c7d191e3d787754ec9a0
Adrien <adrien@arccoza.com>
2017-09-30 15:28:17 +0900

    Add marker SVG icon, refactor code

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,45 @@ import mapTheme from './map-theme.js'
 var print = console.log.bind(console)
 
 
+// Call ready when the DOM has loaded.
+window.addEventListener('load', function(ev) {
+  ready()
+})
+
 var places = [
-  'Durban',
-  'Tokyo',
-  'Bangkok',
+  {
+    name: 'Durban, South Africa',
+    lat: -29.856849, lng: 31.013158
+  },
+  {
+    name: 'uShaka Marine World',
+    address: '1, King Shaka Ave, Point, Point, Durban, 4001, South Africa',
+    web: 'ushakamarineworld.co.za',
+  },
+  {
+    name: 'Surf Riders Food Shack',
+    address: '17 Erskine Terrace, South Beach, Durban, 4001, South Africa',
+    web: null,
+  },
 ]
 
-function AppVM() {
+// REF: https://stackoverflow.com/questions/24413766/how-to-use-svg-markers-in-google-maps-api-v3
+var markerIcon = {
+  path: 'M262.576,0C160.959,0,78.817,82.164,78.817,183.76c0,137.874,183.76,341.393,183.76,341.393s183.76-203.518,183.76-341.393 C446.336,82.164,364.193,0,262.576,0z M262.576,249.404c-36.257,0-65.644-29.387-65.644-65.644 c0-36.17,29.387-65.644,65.644-65.644s65.644,29.474,65.644,65.644C328.22,220.039,298.834,249.404,262.576,249.404z',
+  fillColor: '#ff7ad5',
+  fillOpacity: 1,
+  anchor: {x: 525.153/2, y: 525.153},
+  strokeWeight: 0,
+  scale: 0.1
+}
+
+var mapOptions = {
+  zoom: 11,
+  center: {lat: -29.856849, lng: 31.013158},  // Durban
+  styles: mapTheme,
+}
+
+function AppVM({map}) {
   this.menuShown = ko.observable(true)
   this.menuHidden = ko.computed(() => !this.menuShown())
   this.toggleMenu = () => this.menuShown(!this.menuShown())
@@ -18,46 +50,30 @@ function AppVM() {
   this.places = ko.observableArray(places)
   this.placesSearch = ko.computed(() => {
     var s = this.search()
-
     if (s) {
       return ko.utils.arrayFilter(this.places(), p => p.toUpperCase().indexOf(s.toUpperCase()) != -1)
     }
-
     return this.places()
   })
 }
 
-window.addEventListener('load', function(ev) {
-
-  ko.applyBindings(new AppVM())
-
-})
-
-
 // key: AIzaSyAIThqsGw6NkA5oIJ1Q3nJmQrtA7B8-Uko
 function initMap() {
-  // Basic options for a simple Google Map
-  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-  var mapOptions = {
-    // How zoomed in you want the map to start at (always required)
-    zoom: 11,
+  var map = new google.maps.Map(document.getElementById('map-area'), mapOptions)
 
-    // The latitude and longitude to center the map (always required)
-    center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-    // How you would like to style the map.
-    // This is where you would paste any style found on Snazzy Maps.
-    styles: mapTheme,
-  }
-  var mapElement = document.getElementById('map-area')
-  var map = new google.maps.Map(mapElement, mapOptions)
-
-  // Let's also add a marker while we're at it
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(40.6700, -73.9400),
+    position: new google.maps.LatLng(-29.8645465, 31.0438486),
+    icon: markerIcon,
+    animation: google.maps.Animation.DROP,
     map: map,
     title: 'Snazzy!'
   })
+
+  return map
 }
 
-window.initMap = initMap
+function ready() {
+  var map = initMap()
+  ko.applyBindings(new AppVM({map}))
+}
+
